3a5941980cd8c19171b27a08c3b489d5520163d7
Lou Ssasshole <gt7x32@outlook.com>
2024-01-29 11:49:59 -0500

    adding rest of the files

---
+++ README_Template/readme-template.txt
@@ -0,0 +1,52 @@
+# {Project Title}
+
+{Overview}
+
+## Description
+
+{Description}
+
+## Getting Started
+
+### Dependencies
+
+* {Prerequisites}
+* Windows 10
+
+### Installing
+
+* git clone https://github.com/{Github Username}/{Repo Name}.git
+
+### Executing program
+
+* How to run the program
+
+* {Instructions}
+
+## Help/Support
+
+Any advise for common problems or issues reach out to the maintainer at the following place:
+*The email which is located in GitHub profile 
+
+## Roadmap
+
+{Roadmap overview}
+
+## Authors
+
+{Github Name}  
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## License
+
+This project is licensed under the {License} License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [{Inspiration Repo1}]({Inspiration Repo1 link})
+* [{Inspiration Repo2}]({Inspiration Repo2 link})
---
+++ Template_Readme.py
@@ -0,0 +1,44 @@
+import string
+import os
+
+def load_template():
+    template = os.path.join('README_Template','readme-template.txt')
+    custom_template = []
+    with open(template,'r') as f:
+        mytemplate = f.read()
+        custom_template.append(mytemplate)
+        return custom_template
+    
+def license_choices():
+    filename = [f.split('.')[0] for f in os.listdir('LICENSE_Template')]
+    return filename
+
+def load_license(filename):
+    license_template = os.path.join('LICENSE_Template',f'{filename}.txt')
+    custom_license = []
+    with open(license_template,'r') as f:
+        mytemplate = f.read()
+        custom_license.append(mytemplate)
+        return custom_license
+
+def fill_template(custom_template):
+
+    for line in custom_template:
+        placeholders = [placeholder[1] for placeholder in string.Formatter().parse(line) if placeholder[1]]
+        user_inputs = {placeholder: input(f'Enter a {placeholder}: ') for placeholder in placeholders}
+
+        if 'License' in user_inputs and user_inputs['License'] in license_choices():
+            license_temp = load_license(user_inputs['License'])
+            for lines in license_temp:
+                license_placeholders = [placeholder[1] for placeholder in string.Formatter().parse(lines) if placeholder[1]]
+                license_inputs = {placeholder: input(f'Enter a {placeholder}: ') for placeholder in license_placeholders}
+                custom_license_filled = lines.format(**license_inputs)
+                with open('License','w') as f:
+                    f.write(custom_license_filled)
+
+        custom_temp_filled = line.format(**user_inputs)
+        with open('README.md','w') as f:
+            f.write(custom_temp_filled)
+
+custom_template = load_template()
+fill_template(custom_template)
